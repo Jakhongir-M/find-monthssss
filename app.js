273f0993b3bcb9fdf8eacd9a67dd51227db1891a
Jakhongir Mirzakhmatov <94097414+Jakhongir-M@users.noreply.github.com>
2022-05-20 18:41:15 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,40 @@
+let months = +prompt("12-oy -12-raqam 12-ta raqamdan bittasini kiriting");
+switch (months) {
+  case 1:
+    alert("siz Yanvar oyini kiritingiz");
+    break;
+  case 2:
+    alert("siz fevral oyini kiritingiz");
+    break;
+  case 3:
+    alert(" mart oyini kiritingiz");
+  case 4:
+    alert(" aprel oyini kiritingiz");
+    break;
+  case 5:
+    alert(" may oyini kiritingiz");
+    break;
+  case 6:
+    alert(" iyun oyini kiritingiz");
+    break;
+  case 7:
+    alert(" iyul oyini kiritingiz");
+    break;
+  case 8:
+    alert(" avgust oyini kiritingiz");
+    break;
+  case 9:
+    alert(" sentyabr oyini kiritingiz");
+    break;
+  case 10:
+    alert(" oktyabr oyini kiritingiz");
+    break;
+  case 11:
+    alert("noyabr oyini kiritingiz");
+    break;
+  case 12:
+    alert("yangi yil yaqin dekabr oyi kirdi :D");
+  default:
+    alert("siz kiritkan oy yoq aldama :D");
+    break;
+}
